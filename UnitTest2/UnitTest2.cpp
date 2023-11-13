0c8bb85829bfcb81bedc352f65199ec2a09b1b20
Serhii455 <serhii.maksymiv.ri.2023@lpnu.ua>
2023-11-14 01:38:17 +0200

    my commit

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr_8.1_IT/8.1_it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            {
+                char str[101] = "Hello !!! World !!!";
+
+                int g = Count(str);
+
+                Assert::AreEqual(g, 2);
+            }
+        }
+    };
+}
